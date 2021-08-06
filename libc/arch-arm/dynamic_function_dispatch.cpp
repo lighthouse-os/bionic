9c714deee5175b0a3b533b5f7d279690a1c72070
Jebaitedneko <jebaitedneko@gmail.com>
2021-08-06 10:03:09 +0200

    Bionic: Support kryo585 CPU

--- libc/arch-arm/dynamic_function_dispatch.cpp
+++ libc/arch-arm/dynamic_function_dispatch.cpp
@@ -53,6 +53,7 @@ static constexpr CpuVariantNames cpu_variant_names[] = {
     {"cortex-a76", kCortexA55},
     {"kryo385", kCortexA55},
     {"kryo485", kCortexA55},
+    {"kryo585", kCortexA55},
     {"cortex-a75", kCortexA55},
     {"kryo", kKryo},
     {"cortex-a73", kCortexA55},
